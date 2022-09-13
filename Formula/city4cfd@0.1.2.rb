5ae8ac870dc276f958df498c9b457570afd04f77
GitHub Action <action@github.com>
2022-09-13 15:25:35 +0000

    [Automated commit] push version-specific brew formula for city4cfd version 0.1.2

---
+++ Formula/city4cfd@0.1.2.rb
@@ -0,0 +1,27 @@
+class City4cfdAT012 < Formula
+  desc "Reconstruction of 3D city models tailored to urban CFD simulations"
+  homepage "https://github.com/tudelft3d/city4cfd"
+  url "https://github.com/tudelft3d/city4cfd/archive/0.1.2.tar.gz"
+  sha256 "f8c084c3d213fa33ab7555e61f50544c9acff3a0bfb0d1b7aa2831ffa8f300fb"
+
+  depends_on "cmake" => :build
+  depends_on "cgal"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test val3dity`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
