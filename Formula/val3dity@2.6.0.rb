9af2dfc64734647251b115957323e6b4103d994f
GitHub Action <action@github.com>
2026-01-07 16:24:29 +0000

    [Automated commit] push version-specific brew formula for val3dity version 2.6.0

---
+++ Formula/val3dity@2.6.0.rb
@@ -0,0 +1,28 @@
+class Val3dityAT260 < Formula
+  desc "Validation of 3D geometric primitives according to ISO19107"
+  homepage "https://github.com/tudelft3d/val3dity"
+  url "https://github.com/tudelft3d/val3dity/archive/2.6.0.tar.gz"
+  sha256 "9386f62470e049ac36a092d0bdb95f6ced6f413fc01ab0653d62a83a291cfac3"
+
+  depends_on "cmake" => :build
+  depends_on "cgal"
+  depends_on "geos"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test val3dity`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
